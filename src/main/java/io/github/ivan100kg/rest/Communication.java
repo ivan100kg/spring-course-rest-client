5991cf874278acd464b11acbefb4a0ebd7cf421f
Ivan B <ivar110kg@gmail.com>
2022-07-05 12:41:08 +0400

    Add Communication.java

---
+++ src/main/java/io/github/ivan100kg/rest/Communication.java
@@ -0,0 +1,39 @@
+package io.github.ivan100kg.rest;
+
+import io.github.ivan100kg.rest.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Component
+public class Communication {
+
+    @Autowired
+    private RestTemplate restTemplate;
+    private final String URL = "http://localhost:8080/api/employees";
+
+    public List<Employee> getAllEmployees() {
+        ResponseEntity<List<Employee>> responseEntity =
+                restTemplate.exchange(URL, HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<Employee>>() {});
+        List<Employee> allEmployees = responseEntity.getBody();
+        return allEmployees;
+    }
+
+    public Employee getEmployee(int id) {
+        return null;
+    }
+
+    public void savaEmployee(Employee employee) {
+
+    }
+
+    public void deleteEmployee(int id) {
+
+    }
+}
