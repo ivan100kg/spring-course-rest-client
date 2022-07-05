16b8d9087f645569d76ee33d18d19cd2fc918d1f
Ivan B <ivar110kg@gmail.com>
2022-07-05 12:58:39 +0400

    Add CRUD

--- src/main/java/io/github/ivan100kg/rest/App.java
+++ src/main/java/io/github/ivan100kg/rest/App.java
@@ -12,7 +12,28 @@ public class App {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(MyConfig.class);
         Communication communication = context.getBean("communication", Communication.class);
+
+        // get all Employee
         List<Employee> allEmployees = communication.getAllEmployees();
         allEmployees.forEach(System.out::println);
+
+        System.out.println("-------------------------------------------------------------------");
+
+        // get 1 Employee
+        Employee employee = communication.getEmployee(1);
+        System.out.println(employee);
+
+        System.out.println("-------------------------------------------------------------------");
+
+        // save Employee
+        Employee sveta = new Employee("Sveta", "Petrova", "Sales", 550);
+        sveta.setId(14);
+        communication.saveEmployee(sveta);
+
+        System.out.println("-------------------------------------------------------------------");
+
+        // delete Employee
+        communication.deleteEmployee(14);
+
     }
 }
--- src/main/java/io/github/ivan100kg/rest/Communication.java
+++ src/main/java/io/github/ivan100kg/rest/Communication.java
@@ -20,20 +20,32 @@ public class Communication {
     public List<Employee> getAllEmployees() {
         ResponseEntity<List<Employee>> responseEntity =
                 restTemplate.exchange(URL, HttpMethod.GET, null,
-                        new ParameterizedTypeReference<List<Employee>>() {});
+                        new ParameterizedTypeReference<List<Employee>>() {
+                        });
         List<Employee> allEmployees = responseEntity.getBody();
         return allEmployees;
     }
 
     public Employee getEmployee(int id) {
-        return null;
+        Employee employee = restTemplate.getForObject(URL + "/" + id, Employee.class);
+        return employee;
     }
 
-    public void savaEmployee(Employee employee) {
-
+    public void saveEmployee(Employee employee) {
+        int id = employee.getId();
+        if (id == 0) {
+            ResponseEntity<String> responseEntity =
+                    restTemplate.postForEntity(URL, employee, String.class);
+            System.out.println("New employee was added to DB");
+            System.out.println(responseEntity.getBody());
+        } else {
+            restTemplate.put(URL, employee);
+            System.out.println("Employee with id = " + id + " was updated");
+        }
     }
 
     public void deleteEmployee(int id) {
-
+        restTemplate.delete(URL + "/" + id);
+        System.out.println("Employee with id = " + id + " was deleted from DB");
     }
 }
